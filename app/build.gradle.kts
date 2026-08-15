a8b2f122d2fe054f8cb7cdd523cefdd30c7c6695
bala366 <josuelima2010@hotmail.com>
2026-08-15 18:21:26 -0300

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins { id("com.android.application") }
+android {
+ namespace = "br.com.lotofacil.perimetro11"
+ compileSdk = 35
+ defaultConfig { applicationId = "br.com.lotofacil.perimetro11"; minSdk = 26; targetSdk = 33; versionCode = 1; versionName = "1.0.0" }
+ compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+}
+dependencies {}
